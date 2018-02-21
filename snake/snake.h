8351d735de0a4350f6bd4846b96ca40b8ad2a081
EnriqueJose99 <enrique.galeano99@yahoo.com>
2018-02-21 07:12:07 -0600

    Snake Game en ncurses

---
+++ snake/snake.h
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <ncurses.h>
+#include <cstdlib>
+#ifndef SNAKE_H
+#define SNAKE_H
+struct snakepart{
+		int x,y;
+			snakepart(int col, int row);
+				snakepart();
+};
+
+class snakeclass{
+	int points,del;
+	bool get;
+	char direction;
+	int maxwidth;
+	int maxheight;
+	char partchar;
+	char oldalchar;
+	char etel;
+	snakepart food;
+	std::vector<snakepart> snake;
+
+	void putfood();
+	bool collision();
+	void movesnake();
+
+	public:
+		snakeclass();
+		~snakeclass();
+		void start();
+};
+
+#endif
